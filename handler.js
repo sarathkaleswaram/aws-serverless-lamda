2c1a340cd37d45557527b34f877d096f5ef8fd63
sarathkaleswaram <ksb994@gmail.com>
2019-06-17 22:24:32 +0530

    first commit

---
+++ handler.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const userCreate = require('./users-create.js');
+const usersReadAll = require('./users-read-all.js');
+const userReadOne = require('./users-read-one.js');
+const userUpdate = require('./users-update.js');
+const userDelete = require('./users-delete.js');
+
+module.exports.add = (event, context, callback) => {
+  userCreate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.list = (event, context, callback) => {
+  usersReadAll(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.get = (event, context, callback) => {
+  userReadOne(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.update = (event, context, callback) => {
+  userUpdate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.delete = (event, context, callback) => {
+  userDelete(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
